1549654da946bb45ccdbe3e4c4d7af9a42335cf1
unknown <you@example.com>
2024-05-05 10:43:40 +0200

    fixed some stuff

--- main.js
+++ main.js
@@ -1,26 +1,152 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import { getProject, types } from '@theatre/core';
+import studio from '@theatre/studio';
+studio.initialize();
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// Create a project for the animation
+const project = getProject('TheatreTutorial_1');
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Create a sheet
+const sheet = project.sheet('AnimationScene');
 
-camera.position.z = 5;
+let controls = OrbitControls;
+let camera = THREE.PerspectiveCamera;
+let renderer = THREE.WebGLRenderer;
+let scene = THREE.Scene;
 
-function animate() {
-	requestAnimationFrame( animate );
+init();
+requestAnimationFrame(tick);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+function init() {
+  // Camera
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    10,
+    200,
+  );
+  camera.position.set(40, 10, 40);
 
-	renderer.render( scene, camera );
+  // Scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x292929);
+
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.autoUpdate = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
+  document.body.appendChild(renderer.domElement);
+
+  setupLights();
+  setupOrbitControls();
+  setupEventListeners();
+
+  // ***** setup our scene *******
+
+  // Box
+  const geometry = new THREE.BoxGeometry(10, 10, 10);
+  const material = new THREE.MeshPhongMaterial({ color: 0x049ef4 });
+
+  const box = new THREE.Mesh(geometry, material);
+  box.castShadow = true;
+  box.receiveShadow = true;
+  scene.add(box);
+
+  const planeGeometry = new THREE.CircleGeometry(30, 50);
+  const planeMaterial = new THREE.MeshPhongMaterial({ color: 0xf0f0f0 });
+
+  const floor = new THREE.Mesh(planeGeometry, planeMaterial);
+  floor.rotateX(-Math.PI / 2);
+  floor.translateZ(-10);
+  floor.receiveShadow = true;
+  scene.add(floor);
+
+  // ***** Theatre Config *******
+
+  const boxObj = sheet.object('Box', {
+    rotation: types.compound({
+      xR: types.number(box.rotation.x, { range: [-Math.PI, Math.PI] }),
+      yR: types.number(box.rotation.y, { range: [-Math.PI, Math.PI] }),
+      zR: types.number(box.rotation.z, { range: [-Math.PI, Math.PI] }),
+    }),
+    position: types.compound({
+      x: types.number(0, { nudgeMultiplier: 0.1 }),
+      y: types.number(0, { nudgeMultiplier: 0.1 }),
+      z: types.number(0, { nudgeMultiplier: 0.1 }),
+    }),
+	scale: types.compound({
+	  x: types.number(1, { range: [0.1, 5], step: 0.1 }), // Default scale 1, adjustable from 0.1 to 5
+	  y: types.number(1, { range: [0.1, 5], step: 0.1 }),
+	  z: types.number(1, { range: [0.1, 5], step: 0.1 })
+	})
+  });
+
+  boxObj.onValuesChange((values) => {
+    const { xR, yR, zR } = values.rotation;
+    box.rotation.set(xR, yR, zR);
+    const { x, y, z } = values.position;
+    box.position.set(x, y, z);
+	const { x: scaleX, y: scaleY, z: scaleZ } = values.scale;
+	box.scale.set(scaleX, scaleY, scaleZ);
+  });
+
 }
 
-animate();
+// RAF Update the screen
+function tick() {
+  renderer.render(scene, camera);
+  controls.update();
+  window.requestAnimationFrame(tick);
+}
+
+
+function setupLights() {
+  // ***** Lights ****** //
+  const ambLight = new THREE.AmbientLight(0xfefefe, 0.2);
+  const dirLight = new THREE.DirectionalLight(0xfefefe, 1);
+  dirLight.castShadow = true;
+  dirLight.shadow.mapSize.width = 1024;
+  dirLight.shadow.mapSize.height = 1024;
+  dirLight.shadow.camera.far = 100;
+  dirLight.shadow.camera.near = 1;
+  dirLight.shadow.camera.top = 40;
+  dirLight.shadow.camera.right = 40;
+  dirLight.shadow.camera.bottom = -40;
+  dirLight.shadow.camera.left = -40;
+
+  dirLight.position.set(20, 30, 20);
+  scene.add(ambLight, dirLight);
+}
+
+function setupOrbitControls() {
+  // OrbitControls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableZoom = true;
+  controls.enableDamping = true;
+  controls.autoRotate = false;
+  controls.rotateSpeed = 1;
+  controls.dampingFactor = 0.1;
+  controls.minDistance = 2.4;
+
+}
+
+function setupEventListeners() {
+  // Handle `resize` events
+  window.addEventListener(
+    'resize',
+    function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    },
+    false,
+  );
+}
